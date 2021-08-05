0b2a876bb8809104f27ca316ff18685c475e1063
UzairAjml <uzair@fecundity.io>
2021-08-05 11:53:38 +0500

    react basic app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+  import React, { useState } from 'react'
+ import Tweet from './Tweet'
+ import './App.css';
+ 
+ 
+ function App ()  {
+
+
+  const [villians,setVillians]=useState([
+
+    {name:'LOKI', moto:'Do you truly think so little of me?'  },
+    {name:'THANOS', moto:'You Should Have Gone For The Head'  },
+    {name:'HELA', moto:'Our destiny is to rule over all others. And I am here to restore that power' },
+    {name:'ULTRON', moto:' As I always say, keep your friends rich, and your enemies rich, and then find out which is which'  }
+  ])
+
+   return (
+     <div className='App'>
+         
+        {villians.map(villian=>(
+          <Tweet name={villian.name} moto={villian.moto}/>
+        ))}
+     </div>
+   )
+ }
+ 
+ export default App
+   
---
+++ src/Tweet.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+import './App.css';
+
+const Tweet = ({name,moto}) => {
+
+
+    let  [like,setLike ]=useState(0); 
+
+    const increment=()=>{
+        setLike(like++);
+    }
+     
+
+    return (
+        <div className='Tweet'>
+            <h1>{name}</h1>
+            <p>{moto}</p>
+            <div className='Likes'> 
+            <button   id='btn' onClick={increment}>Likes</button>
+            <h2>{like}</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Tweet
